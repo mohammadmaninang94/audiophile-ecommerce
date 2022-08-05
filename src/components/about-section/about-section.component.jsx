042dbb1707ba26c70c9f3a091fdcdeb5dcd6441c
Mohammad Maninang <mohammadmaninang94@gmail.com>
2022-08-05 16:29:43 +0800

    Finished mobile styling of home page

---
+++ src/components/about-section/about-section.component.jsx
@@ -0,0 +1,15 @@
+import './about-section.styles.scss';
+
+const AboutSection = () => {
+    return (
+        <section className='about-section'>
+            <div>
+                <h2>Bringing you the <span>best</span> audio gear</h2>
+                <p>Located at the heart of New York City, Audiophile is the premier store for high end headphones, earphones, speakers, and audio accessories. We have a large showroom and luxury demonstration rooms available for you to browse and experience a wide range of our products. Stop by our store to meet some of the fantastic people who make Audiophile the best place to buy your portable audio equipment.</p>
+            </div>
+            <img src='/images/shared/mobile/image-best-gear.jpg' alt='best gear' />
+        </section>
+    );
+};
+
+export default AboutSection;
--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -2,12 +2,16 @@ import { Fragment } from 'react';
 
 import CategorySection from '../../components/category-section/category-section.component';
 import LatestSection from '../../components/latest-section/latest-section.component';
+import AboutSection from '../../components/about-section/about-section.component';
+
+import './home.styles.scss';
 
 const Home = () => {
     return (
         <Fragment>
             <CategorySection />
             <LatestSection />
+            <AboutSection />
         </Fragment>
     );
 };
