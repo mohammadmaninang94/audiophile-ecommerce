482429f61673ba5746dc20be5921123fc63fb911
Mohammad Maninang <mohammadmaninang94@gmail.com>
2022-09-02 13:57:27 +0800

    used cartContext in checkout modal

--- src/components/checkout-modal/checkout-modal.component.jsx
+++ src/components/checkout-modal/checkout-modal.component.jsx
@@ -2,6 +2,7 @@
 import { useState, useContext } from 'react';
 
 import { AppContext } from '../../contexts/app.context';
+import { CartContext } from '../../contexts/cart.context';
 
 import CustomCta, { ctaTag, ctaType } from '../custom-cta/custom-cta.component';
 
@@ -9,6 +10,7 @@ import './checkout-modal.styless.scss';
 
 const CheckoutModal = () => {
     const { toggleCheckoutModal } = useContext(AppContext);
+    const { items, itemsTotal } = useContext(CartContext);
     const [viewLess, setViewLess] = useState(true);
 
     const viewitemsHandler = () => {
@@ -24,36 +26,25 @@ const CheckoutModal = () => {
                 <div className='checkout-modal__breakdown'>
                     <div className='checkout-modal__items'>
                         <div className={`checkout-modal__item-wrapper ${viewLess ? '' : 'checkout-modal__item-wrapper--more'}`}>
-                            <div className='checkout-modal__item'>
-                                <img src='/images/cart/image-xx99-mark-two-headphones.jpg' alt='XX99 MK II' />
-                                <div>
-                                    <strong>XX99 MK II</strong>
-                                    <span>$2,999</span>
-                                </div>
-                                <span className='checkout-modal__item-qty'>x1</span>
-                            </div>
-                            <div className='checkout-modal__item'>
-                                <img src='/images/cart/image-xx59-headphones.jpg' alt='XX59' />
-                                <div>
-                                    <strong>XX59</strong>
-                                    <span>$899</span>
-                                </div>
-                                <span className='checkout-modal__item-qty'>x2</span>
-                            </div>
-                            <div className='checkout-modal__item'>
-                                <img src='/images/cart/image-yx1-earphones.jpg' alt='YX1' />
-                                <div>
-                                    <strong>YX1</strong>
-                                    <span>$599</span>
-                                </div>
-                                <span className='checkout-modal__item-qty'>x1</span>
-                            </div>
+                            {items.map((item) => {
+                                const { id, name, price, quantity, thumbnailImage } = item;
+                                return (
+                                    <div key={id} className='checkout-modal__item'>
+                                        <img src={thumbnailImage} alt={name} />
+                                        <div>
+                                        <strong>{name}</strong>
+                                        <span>$ {price.toLocaleString('en-US')}</span>
+                                        </div>
+                                        <span className='checkout-modal__item-qty'>x{quantity}</span>
+                                    </div>
+                                )
+                            })}
                         </div>
                         <CustomCta type="button" tag={ctaTag.BUTTON} ctaType={ctaType.LINK} handleClick={viewitemsHandler}>{viewLess ? 'and 2 other item(s)' : 'View less'}</CustomCta>
                     </div>
                     <div className='checkout-modal__total-box'>
                         <h6>grand total</h6>
-                        <h5>$ 5,446</h5>
+                        <h5>$ {itemsTotal.toLocaleString('en-US')}</h5>
                     </div>
                 </div>
                 <CustomCta href='/' tag={ctaTag.DEFAULT} ctaType={ctaType.PRIMARY} handleClick={toggleCheckoutModal}>back to home</CustomCta>
